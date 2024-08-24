19f6e1e5ee6d411b48ca11e1acf51bf6847c79b6
Ayush Singh <96381373+Ayush59699@users.noreply.github.com>
2024-08-24 12:43:11 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -77,7 +77,7 @@ function Home2() {
               
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/ayush-singh-560a29284/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
