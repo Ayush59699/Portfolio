051d57c630acf85c86df0539841dfca17de426b7
Ayush59699 <21f3001194@ds.study.iitm.ac.in>
2026-05-02 12:28:11 +0530

    fix: remove submodule and track files properly

---
+++ Portfolio/src/components/About/Techstack.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import { FaVuejs } from "react-icons/fa";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiPython,
+  DiGit,
+  DiJava,
+} from "react-icons/di";
+import {
+  SiNextdotjs,
+  SiPostgresql,
+  SiFlask,
+  SiGnubash,
+  SiBootstrap,
+  SiMysql,
+  SiSqlite,
+} from "react-icons/si";
+
+
+ 
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <FaVuejs/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <SiFlask/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGnubash/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <SiBootstrap/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSqlite />
+      </Col>
+    </Row>
+
+  
+
+
+  );
+}
+
+export default Techstack;
