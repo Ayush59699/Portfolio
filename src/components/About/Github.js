d196ac4a90df911949576aa042cf229d4951b2e7
Ayush59699 <21f3001194@ds.study.iitm.ac.in>
2026-05-02 14:21:34 +0530

    Replace GitHub calendar with Codeforces stats widget

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,19 +1,16 @@
 import React from "react";
-import GitHubCalendar from "react-github-calendar";
 import { Row } from "react-bootstrap";
 
 function Github() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        Days I <strong className="purple">Code</strong>
+        My <strong className="purple">Codeforces</strong> Stats
       </h1>
-      <GitHubCalendar
-        username="Ayush59699"
-        blockSize={15}
-        blockMargin={5}
-        color="#c084f5"
-        fontSize={16}
+      <img
+        src="https://codeforces-readme-stats.vercel.app/api/card?username=Ayush687141&theme=dark"
+        alt="Codeforces Stats"
+        style={{ width: "100%", maxWidth: "500px", display: "block", margin: "0 auto" }}
       />
     </Row>
   );
